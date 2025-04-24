b627aeb118e544716698c939a3a4b17f42273397
marmoresimone <marmoresimone14@gmail.com>
2025-04-24 15:07:43 +0200

    test simone 1

--- app/src/main/java/com/example/firsttry/MainActivity.kt
+++ app/src/main/java/com/example/firsttry/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     FirstTryTheme {
-        Greeting("simone")
+        Greeting("simone2")
     }
 }
